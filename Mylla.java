020a054f3e0f7a28e3d13ecb6b7c855055dbb20c
bergurlogi <bergurlogi@hotmail.com>
2013-11-21 15:47:48 +0000

    Bætti við 3 prentskipunum

--- Mylla.java
+++ Mylla.java
@@ -4,6 +4,9 @@ public class Mylla {
 	public static void main(String[] args)
 	{
 		System.out.println("hhhhhhhhhh");
+		System.out.println("hhhhhhhhhh");
+		System.out.println("hhhhhhhhhh");
+		System.out.println("hhhhhhhhhh");
 	}
 }
 	
